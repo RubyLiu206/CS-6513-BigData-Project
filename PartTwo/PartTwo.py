7beaa3c8fa83beba586122f3cce7bafe29a68618
RubyLiu206 <xl2590@nyu.edu>
2019-12-01 17:59:56 -0500

    ADD: true type

--- PartTwo/PartTwo.py
+++ PartTwo/PartTwo.py
@@ -6,6 +6,7 @@ from math import sqrt
 from operator import add
 from os.path import join, isfile, dirname
 from pyspark import SparkConf, SparkContext
+import csv
 from csv import reader
 import re
 from pyspark.sql import SQLContext
@@ -26,45 +27,54 @@ def get_file_column_name():
 	return file_list,column_list
 
 
-def read_into_RDD(file_name, column_name):
-	sc = SparkContext()
-	sqlContext = SQLContext(sc)
+def read_into_RDD(sc, file_name, column_name):
+	#sqlContext = SQLContext(sc)
 	#5694-9szk.Business_Website_or_Other_URL.txt.gz for test
 	column_name = str(column_name)
 	column_name = column_name.replace("_", " ")
 	file_path = "/user/hm74/NYCOpenData/"+str(file_name)
-
-	lines = sc.textFile(file_path,1)
-	print("ther are:" + "\t" + str(lines.count()) + "lines in the file")
-	
-	lines = lines.map(lambda x: x.split('\t'))
+	lines = sc.textFile(file_path, 1).mapPartitions(lambda x: csv.reader(x, delimiter='\t', quotechar='"'))
+	# get the header which is the column name
 	header = lines.first()
+	# print("the column name are: ", header)
+	# modify the dataset without the header row
+	print("ther are:" + "\t" + str(lines.count()) + "lines in the file")
 	header_string = list(header)
-
-	specific_column_index = header_string.index(column_name)
-	print("the column names are :" + str(header))
-	print("the specific column:" + str(specific_column_index))
-
-	lines_without_header = lines.filter(lambda line: line!=header)
-	
-	specific_column = lines_without_header.map(lambda x: x[specific_column_index])
-	return specific_column
+	if column_name in header_string:
+		specific_column_index = header_string.index(column_name)
+		print("the column names are :" + str(header))
+		print("the specific column:" + str(specific_column_index))
+		lines_without_header = lines.filter(lambda line: line!=header)
+		specific_column = lines_without_header.map(lambda x: x[specific_column_index])
+		return specific_column
+	else:
+		return None
 
 
 def dealing_with_column(column):	
 	data_collect = column.map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y).sortByKey(lambda x: x[1])
-	print(data_collect.take(5))
+	return data_collect.take(5)
+
+def write_into_df(file_list,column_list, item):
+	dataset = pd.DataFrame({'file': file_list, 'column_name': column_list, 'freq_item':item})
+	#print(dataset)
+	dataset.to_csv('true_type.csv', encoding='gbk')
+
+
 
 
 file_list, column_list = get_file_column_name()
-column = read_into_RDD(file_list[0], column_list[0])
-dealing_with_column(column)
-list_ =[list(file_list),list(column_list)]
-name = ['dataset','column_name']
-dataset = pd.DataFrame({'file': file_list, 'column_name':column_list})
-print(dataset)
-dataset.to_csv('true_type.csv', encoding = 'gbk')
+item = []
+sc = SparkContext()
 
-print(file_list)
-print(column_list)
+for i in range(274):
+	column = read_into_RDD(sc, file_list[i], column_list[i])
+	if column != None:
+		each_item = dealing_with_column(column)
+		item.append(each_item)
+	else:
+		item.append([0])
+
+
+write_into_df(file_list,column_list, item)
 
